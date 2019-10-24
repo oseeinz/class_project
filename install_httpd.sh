62182aac074a0f548f93a67233144739bbc13176
Cloud User <ec2-user@ip-10-0-0-100.ec2.internal>
2019-10-24 06:12:22 +0000

    fixed permission issue

--- install_httpd.sh
+++ install_httpd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo yum install httpd -y
-sudo echo "Automation for the people" > /var/www/html/index.html
+echo "Automation for the people" | sudo tee /var/www/html/index.html
 sudo systemctl start httpd.service
 sudo systemctl enable httpd.service
